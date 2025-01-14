60be534cd824982bd879d87b9233506ccd9a8a4c
Bank Athiwat <athiwat.sitthinanwatthana@gmail.com>
2025-01-14 11:19:58 +0700

    update - config and structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { SideMenuComponent } from './shared/side-menu/side-menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [
+		RouterOutlet,
+		HeaderComponent,
+		FooterComponent,
+		SideMenuComponent,
+	],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'trading-bot-fontend-angular';
+	title = 'trading-bot-fontend-angular';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		redirectTo: '/dashboard',
+		pathMatch: 'full',
+	},
+	{
+		path: 'dashboard',
+		title: 'Dashboard',
+		component: DashboardComponent,
+	},
+	{
+		path: 'profile',
+		title: 'Profile',
+		component: ProfileComponent,
+	},
+];
